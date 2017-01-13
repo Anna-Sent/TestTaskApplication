0d2d7f2f4c207403d9603a8e3985b7f9b62430dd
Anna <anna.sent@gmail.com>
2017-01-13 17:27:02 +0400

    lombok, parceler

--- app/src/main/java/com/anna/sent/soft/testtaskapplication/mvp/models/Employee.java
+++ app/src/main/java/com/anna/sent/soft/testtaskapplication/mvp/models/Employee.java
@@ -1,57 +1,55 @@
 package com.anna.sent.soft.testtaskapplication.mvp.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import org.parceler.ParcelConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Employee implements Parcelable {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+
+@org.parceler.Parcel
+@EqualsAndHashCode
+public class Employee {
     @SerializedName("f_name")
     @Expose
-    private String firstName;
+    @NonNull
+    @Getter
+    String firstName;
 
     @SerializedName("l_name")
     @Expose
-    private String lastName;
+    @NonNull
+    @Getter
+    String lastName;
 
     @SerializedName("birthday")
     @Expose
-    private String birthday;
+    @NonNull
+    @Getter
+    String birthday;
 
     @SerializedName("avatr_url")
     @Expose
-    private String imageUrl;
+    @NonNull
+    @Getter
+    String imageUrl;
 
     @SerializedName("specialty")
     @Expose
-    private List<Speciality> specialities = new ArrayList<>();
-
-    public final static Parcelable.Creator<Employee> CREATOR = new Creator<Employee>() {
-        public Employee createFromParcel(Parcel in) {
-            return new Employee(in);
-        }
-
-        public Employee[] newArray(int size) {
-            return new Employee[size];
-        }
-    };
+    @NonNull
+    @Getter
+    List<Speciality> specialities = new ArrayList<>();
 
     public Employee() {
     }
 
-    public Employee(Parcel in) {
-        firstName = in.readString();
-        lastName = in.readString();
-        birthday = in.readString();
-        imageUrl = in.readString();
-        in.readList(specialities, Speciality.class.getClassLoader());
-    }
-
+    @ParcelConstructor
     public Employee(String firstName, String lastName, String birthday, String imageUrl, List<Speciality> specialities) {
         super();
         this.firstName = firstName;
@@ -62,28 +60,8 @@ public class Employee implements Parcelable {
             this.specialities = new ArrayList<>(specialities);
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
     public boolean hasImage() {
         return imageUrl != null && imageUrl.length() > 0;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public List<Speciality> getSpecialities() {
-        return specialities;
     }
 
     public void addSpeciality(Speciality speciality) {
@@ -91,47 +69,7 @@ public class Employee implements Parcelable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Employee employee = (Employee) o;
-
-        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null)
-            return false;
-        if (lastName != null ? !lastName.equals(employee.lastName) : employee.lastName != null)
-            return false;
-        if (birthday != null ? !birthday.equals(employee.birthday) : employee.birthday != null)
-            return false;
-        if (imageUrl != null ? !imageUrl.equals(employee.imageUrl) : employee.imageUrl != null)
-            return false;
-        return specialities != null ? specialities.equals(employee.specialities) : employee.specialities == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = firstName != null ? firstName.hashCode() : 0;
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
-        result = 31 * result + (specialities != null ? specialities.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return new GsonBuilder().setPrettyPrinting().create().toJson(this);
-    }
-
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(firstName);
-        dest.writeString(lastName);
-        dest.writeString(birthday);
-        dest.writeString(imageUrl);
-        dest.writeList(specialities);
-    }
-
-    public int describeContents() {
-        return 0;
     }
 }
--- app/src/main/java/com/anna/sent/soft/testtaskapplication/mvp/models/Speciality.java
+++ app/src/main/java/com/anna/sent/soft/testtaskapplication/mvp/models/Speciality.java
@@ -1,83 +1,42 @@
 package com.anna.sent.soft.testtaskapplication.mvp.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Speciality implements Parcelable {
+import org.parceler.ParcelConstructor;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+
+@org.parceler.Parcel
+@EqualsAndHashCode
+public class Speciality {
     @SerializedName("specialty_id")
     @Expose
-    private Integer specialityId;
+    @NonNull
+    @Getter
+    Integer specialityId;
 
     @SerializedName("name")
     @Expose
-    private String name;
-
-    public final static Parcelable.Creator<Speciality> CREATOR = new Creator<Speciality>() {
-        public Speciality createFromParcel(Parcel in) {
-            return new Speciality(in);
-        }
-
-        public Speciality[] newArray(int size) {
-            return new Speciality[size];
-        }
-    };
+    @NonNull
+    @Getter
+    String name;
 
     public Speciality() {
     }
 
-    public Speciality(Parcel in) {
-        specialityId = in.readInt();
-        name = in.readString();
-    }
-
+    @ParcelConstructor
     public Speciality(Integer specialityId, String name) {
         super();
         this.specialityId = specialityId;
         this.name = name;
     }
 
-    public Integer getSpecialityId() {
-        return specialityId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Speciality speciality = (Speciality) o;
-
-        if (specialityId != null ? !specialityId.equals(speciality.specialityId) : speciality.specialityId != null)
-            return false;
-        return name != null ? name.equals(speciality.name) : speciality.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = specialityId != null ? specialityId.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
     @Override
     public String toString() {
         return new GsonBuilder().setPrettyPrinting().create().toJson(this);
-    }
-
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(specialityId);
-        dest.writeString(name);
-    }
-
-    public int describeContents() {
-        return 0;
     }
 }
